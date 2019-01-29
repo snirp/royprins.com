b9f6051c3f83694d294f271f035082b5f95665d3
Roy Prins <prinsroy@gmail.com>
2019-01-29 21:12:19 +0100

    Add action for redirect

--- src/components/ContactForm.jsx
+++ src/components/ContactForm.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
 export default () => (
-  <form name="contact" method="POST" data-netlify="true">
+  <form name="contact" method="POST" data-netlify="true" action="/">
     <p>
       <label>Your Name: <input type="text" name="name" /></label>   
     </p>
