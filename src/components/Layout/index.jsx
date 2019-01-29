014d454113f4ae40d81733ef867c5d95af84d8f9
Roy Prins <prinsroy@gmail.com>
2019-01-29 18:04:11 +0100

    Fix react import

--- src/components/Layout/index.jsx
+++ src/components/Layout/index.jsx
@@ -1,4 +1,4 @@
-import React from 'React';
+import React from 'react';
 import './style.css';
 
 import SideMenu from '../SideMenu';
--- src/components/SideMenu.jsx
+++ src/components/SideMenu.jsx
@@ -1,4 +1,4 @@
-import React from 'React';
+import React from 'react';
 import styled from '@emotion/styled';
 
 const Overlay = styled.div`
