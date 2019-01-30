b42f7eb4256b1ce1c161f8e67722a90fcbf665fb
Roy Prins <prinsroy@gmail.com>
2019-01-30 13:01:09 +0100

    Clean up form code

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,47 +1,40 @@
 import React from 'react';
 import axios from 'axios';
 
-const encode = data =>
-  Object.keys(data)
-    .map(key => `${encodeURIComponent(key)}=${encodeURIComponent(data[key])}`)
-    .join('&');
-
 export default class Comment extends React.Component {
-  state = {
-    submitted: false
-  };
+  constructor(props){
+    super(props)
+    this.state = {
+      submitted: false
+    };
+  }
+
+  formName = 'contact';
 
   handleSubmit = (e) => {
     e.preventDefault()
     const formData = new FormData(e.target);
-    formData.append('form-name', 'contact');
-    const params = Array.from(formData.entries()).map(e => 
-      encodeURIComponent(e[0]) + "=" + encodeURIComponent(e[1])
+    formData.append('form-name', this.formName);
+    const params = Array.from(formData.entries()).map(entry => 
+      encodeURIComponent(entry[0]) + "=" + encodeURIComponent(entry[1])
     ).join('&');
     axios.post('/', params)
     .then(res => {
-      console.log(params)
       this.setState({submitted: true})
+      // Other stuff here: success message
     })
     .catch((error) => {
       console.log(error);
+      // Error handling here
     });
   }
 
   render() {
     return (
-  <form name="contact" onSubmit={this.handleSubmit} data-netlify="true" action="/">
-    <p>
-      <label>NAME<input type="text" name="name" /></label>   
-    </p>
-    <p>
-      <label>EMAIL <input type="email" name="email" /></label>
-    </p>
-    <p>
-      <label>MESSAGE <textarea name="message"></textarea></label>
-    </p>
-    <p>
-      <button type="submit">Send</button>
-    </p>
-  </form>
+      <form name={this.formName} onSubmit={this.handleSubmit} data-netlify="true" action="/">
+        <label>name<input type="text" name="name" /></label>   
+        <label>email<input type="email" name="email" /></label>
+        <label>message<textarea name="message"></textarea></label>
+        <button type="submit">Send</button>
+      </form>
 )}};
